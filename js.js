3d30976d84a21e9102d7a4f327303550918daac4
DuyTan <newminer1122003@gmail.com>
2022-05-01 16:24:10 +0700

    Add files via upload

--- js.js
+++ js.js
@@ -1,17 +1,3 @@
-$(function() {
-  $( "#button" ).click(function() {
-    $( "#button" ).addClass( "onclic", 250, validate);
-  });
-
-  function validate() {
-    setTimeout(function() {
-      $( "#button" ).removeClass( "onclic" );
-      $( "#button" ).addClass( "validate", 450, callback );
-    }, 2250 );
-  }
-    function callback() {
-      setTimeout(function() {
-        $( "#button" ).removeClass( "validate" );
-      }, 1250 );
-    }
-  });
+$( ".cta" ).click(function() {
+  $( ".transition").toggleClass( "anim-trans" );
+});
